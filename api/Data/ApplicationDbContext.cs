70a26b36b31afdff12a874899287a8c5734956b2
seda <seda.diriker@gmail.com>
2024-07-24 13:51:58 +0300

    ApplicationDbContext sınıfı oluşturuldu

---
+++ api/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using api.Models;
+
+namespace api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+    }
+}
