17c6bb7850f52c2def73b69dc7826071b53dd31c
Seda Diriker <seda.diriker@gmail.com>
2024-07-27 13:32:01 +0300

    düzeltme

--- PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
+++ PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a9df1cc0a067c29676885e3d9e7d0ebe294318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a12dbd798a913b02174e6560976824ecd35e71")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
